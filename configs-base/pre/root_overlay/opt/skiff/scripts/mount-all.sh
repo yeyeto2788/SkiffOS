df0b882324613fb1747f5f802f7f0114f67ab523
Christian Stewart <christian@paral.in>
2022-06-10 15:12:11 -0700

    configs-base: call udevadm trigger in mount script
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs-base/pre/root_overlay/opt/skiff/scripts/mount-all.sh
+++ configs-base/pre/root_overlay/opt/skiff/scripts/mount-all.sh
@@ -178,49 +178,15 @@ else
     fi
 fi
 
-hostname -F /etc/hostname # change transient hostname
-systemctl daemon-reload
+# Change current hostname
+hostname -F /etc/hostname
 
-# Attempt to resize disk, if necessary.
-# Note: this is an online resize, no re-mount required.
-RESIZE_DISK=(embiggen-disk -verbose -ignore-resize-partition)
-if [ -n "${DISABLE_RESIZE_PARTITION}" ]; then
-    RESIZE_DISK+=(-no-resize-partition)
-fi
-if [ -z "${DISABLE_RESIZE_PERSIST}" ]; then
-    echo "Resizing ${PERSIST_MNT} with embiggen-disk..."
-    # This will only work with /dev/ paths.
-    if [ -b ${PERSIST_DEVICE} ]; then
-        # Sometimes the path does not appear in the mounts list.
-        #
-        # Workaround: temporarily mount it so resize2fs realizes it needs to
-        # do an online resize. Otherwise returns an error.
-        ROOT_MOUNT_SOURCE=$(findmnt --target / --first-only -n -o "SOURCE")
-        if [ -n "${FORCE_RESIZE_PERSIST_MOUNT}" ] || \
-               [[ "${ROOT_MOUNT_SOURCE}" != "${PERSIST_DEVICE}" ]] || \
-               ! findmnt --source ${PERSIST_DEVICE} --first-only; then
-            echo "Temporarily mounting ${PERSIST_DEVICE} to force online resize..."
-            RESIZE_DISK_MTPT=/mnt/.persist-resize
-            mkdir -p ${RESIZE_DISK_MTPT}
-            mount ${PERSIST_DEVICE} ${RESIZE_DISK_MTPT} || true
-        fi
-        echo "Resizing with embiggen-disk via ${PERSIST_DEVICE}..."
-        if ${RESIZE_DISK[@]} ${PERSIST_DEVICE}; then
-            PERSIST_RESIZED="true"
-        fi
-        if [ -n "${RESIZE_DISK_MTPT}" ]; then
-            echo "Unmounting ${RESIZE_DISK_MTPT}..."
-            umount ${RESIZE_DISK_MTPT} || true
-        fi
-    fi
-    if [ -z "${PERSIST_RESIZED}" ]; then
-        if ! ${RESIZE_DISK[@]} ${PERSIST_MNT}; then
-            echo "Failed to resize persist, continuing anyway."
-        else
-            PERSIST_RESIZED="true"
-        fi
-    fi
-fi
+# Ensure any udev rules are processed.
+udevadm control --reload || true
+udevadm trigger || true
+
+# Ensure any new systemd units are loaded.
+systemctl daemon-reload
 
 # Run any additional final setup scripts.
 for i in ${EXTRA_SCRIPTS_DIR}/*.sh ; do
@@ -230,7 +196,3 @@ for i in ${EXTRA_SCRIPTS_DIR}/*.sh ; do
         fi
     fi
 done
-
-# Probe udev to make sure any new kernel modules are picked up.
-systemctl daemon-reload
-systemctl restart --no-block systemd-udev-trigger.service || true
